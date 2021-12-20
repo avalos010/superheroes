bd344ca56562be0fb49e719cb56eb425c9d4a1f6
avalos010 <luizavalos40@gmail.com>
2021-12-19 18:51:05 -0600

    cleaned up uneccesary code

--- src/Components/SuperHero.jsx
+++ src/Components/SuperHero.jsx
@@ -1,13 +1,8 @@
-import React,{useEffect} from 'react'
-import {getAllSuperheroes} from '../Api/Api'
-import {Switch,Link,Route,useParams} from 'react-router-dom';
+import React from 'react'
+import {useParams} from 'react-router-dom';
 
 
 function SuperHero(props) {
-
-    useEffect(() => {
-        // getAllSuperheroes()
-    }, [])
     return (
         <div>
             <h1>SuperHero</h1>
